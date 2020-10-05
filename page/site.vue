2525e3a2b5afc26044fadd18bff8a41c382366d9
极光萝卜 <jglb@vip.qq.com>
2020-10-05 09:53:06 +0800

    update

--- page/site.vue
+++ page/site.vue
@@ -13,8 +13,7 @@
                 sites:[
                     { title:"博客备份",url:"https://blogbak.ijglb.com"},
                     { title:"Steam上的中文Galgame",url:"https://steamgalgame.com"},
-                    { title:"萝卜の图站",url:"https://img.ijglb.com"},
-                    { title:"服务器监控",url:"https://status.ijglb.com"}
+                    { title:"萝卜の图站",url:"https://img.ijglb.com"}
                 ]
             }
         },
